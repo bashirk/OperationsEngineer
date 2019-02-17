0ee184308cb1b61d19723ee53e6c2434fca4c2e1
Korede Bashir <bashirkorede@gmail.com>
2019-02-17 08:44:59 +0100

    10. Added functional UI for looking at policies and their invoices

--- accounting/views.py
+++ accounting/views.py
@@ -1,11 +1,15 @@
 # You will probably need more methods from flask but this one is a good start.
-from flask import render_template
+from flask import render_template, jsonify, json, Response
 
 # Import things from Flask that we need.
 from accounting import app, db
 
 # Import our models
-from models import Contact, Invoice, Policy
+from models import Contact, Invoice, Policy, Payment
+from datetime import datetime, date
+
+ # Import for SQL exception
+import sqlalchemy
 
 # Routing for the server.
 @app.route("/")
